737b0dd98d6769e922f1abc738d6d95e123dc298
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-15 00:16:29 -0400

    Added more Gradle files

---
+++ src/main/java/serikov/dmitrii/todotoday/TodoTodayApplication.java
@@ -0,0 +1,12 @@
+package serikov.dmitrii.todotoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTodayApplication.class, args);
+	}
+}
