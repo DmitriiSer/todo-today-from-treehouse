f8dfaa74a72a41fbe972780160b12a3dc4731273
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-18 23:04:15 -0400

    Minor layout changes

--- src/main/java/serikov/dmitrii/todotoday/TodoTodayApplication.java
+++ src/main/java/serikov/dmitrii/todotoday/TodoTodayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoTodayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoTodayApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TodoTodayApplication.class, args);
+  }
 }
