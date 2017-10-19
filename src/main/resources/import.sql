d450a25b94ae6adab8ea3e3a1cb89e7704e139df
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-18 22:19:19 -0400

    Created a SQL file that will initialize DB data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into task (complete, description) values (true, 'My initial task');
+insert into task (complete, description) values (false, 'Buy groceries');
+insert into task (complete, description) values (false, 'Learn Spring Framework');
+insert into task (complete, description) values (false, 'Feed the dog');
