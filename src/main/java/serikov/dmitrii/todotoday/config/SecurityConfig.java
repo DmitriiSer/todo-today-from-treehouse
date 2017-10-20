ecb173df68fc22b24c44f6033b128a9538ebc5d5
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-19 23:11:30 -0400

    Added security configuration. All non-authorized requests are being redirected to the Login page

---
+++ src/main/java/serikov/dmitrii/todotoday/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package serikov.dmitrii.todotoday.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import serikov.dmitrii.todotoday.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userService);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/static/**");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .anyRequest().hasRole("USER")
+        .and()
+        .formLogin().loginPage("/login").permitAll()
+        .successHandler(loginSuccessHandler())
+        .failureHandler(loginFailureHandler());
+  }
+
+  private AuthenticationSuccessHandler loginSuccessHandler() {
+    return (request, response, authentication) -> response.sendRedirect("/");
+  }
+
+  private AuthenticationFailureHandler loginFailureHandler() {
+    return (request, response, authentication) -> response.sendRedirect("/login");
+  }
+
+}
