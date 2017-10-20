d274a9dbc9a2d0da2da70fb85ba476312546d538
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-19 23:13:41 -0400

    Added Task service and its implementation. Using autowired TaskDao to find all tasks.

---
+++ src/main/java/serikov/dmitrii/todotoday/service/TaskServiceImpl.java
@@ -0,0 +1,24 @@
+package serikov.dmitrii.todotoday.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import serikov.dmitrii.todotoday.dao.TaskDao;
+import serikov.dmitrii.todotoday.model.Task;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+  @Autowired
+  private TaskDao taskDao;
+
+  @Override
+  public List<Task> findAll() {
+    Iterable<Task> iterable= taskDao.findAll();
+    List<Task> tasks =new ArrayList<>();
+    iterable.forEach(tasks::add);
+    return tasks;
+  }
+}
