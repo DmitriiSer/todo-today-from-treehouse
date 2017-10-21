0387a999cab1a7dafb64b2af9b7ca44aa7b2599e
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-20 23:22:04 -0400

    Importing 1 role and 2 users on application start

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,11 @@
+-- Insert roles
+insert into role (name) values ('ROLE_USER');
+
+-- Insert users
+insert into user (username, password, enabled, role_id) values ('user', 'password', true, 1);
+insert into user (username, password, enabled, role_id) values ('user2', 'password', true, 1);
+
+-- Insert tasks
 insert into task (complete, description) values (true, 'My initial task');
 insert into task (complete, description) values (false, 'Buy groceries');
 insert into task (complete, description) values (false, 'Learn Spring Framework');
