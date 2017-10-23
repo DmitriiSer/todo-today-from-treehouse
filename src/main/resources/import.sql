804534572ff769645bef026fb0d55bfeeb473bb4
DmitriiSer <dmitrii.serikov@gmail.com>
2017-10-22 21:58:42 -0400

    Encrypting passwords using BCrypt encoder. Added CSRF protection.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,9 @@
 insert into role (name) values ('ROLE_USER');
 
 -- Insert users
-insert into user (username, password, enabled, role_id) values ('user', 'password', true, 1);
-insert into user (username, password, enabled, role_id) values ('user2', 'password', true, 1);
+-- Passwords are 'password'
+insert into user (username, password, enabled, role_id) values ('user', '$2a$10$P8Xp2LMw1hWA3VWJnuhw3OvEbTj3CWdDys9msWWaBxQHCTTFT0rEG', true, 1);
+insert into user (username, password, enabled, role_id) values ('user2', '$2a$10$P8Xp2LMw1hWA3VWJnuhw3OvEbTj3CWdDys9msWWaBxQHCTTFT0rEG', true, 1);
 
 -- Insert tasks
 insert into task (complete, description, user_id) values (true, 'My initial task', 1);
